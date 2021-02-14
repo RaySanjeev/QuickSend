24a2b6a0e931a6845bb1e4556675d5ad1f699e09
Sanjeev Ray <sanjeevat1996@gmail.com>
2021-02-14 12:46:56 +0530

    Deployment changes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-dotenv.config({ path: './config.env' });
-
-const app = require('./app');
-
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-  console.log(`Server is listening to port: ${port}`);
+// HANDLING ANY OTHER ASYNC ERRORS
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting Dowm.....');
+  console.log(err.name, err.message);
+  process.exit(1);
 });
+
+dotenv.config({ path: './config.env' });
 
 const DB = process.env.DB.replace('<password>', process.env.DB_PASSWORD);
 mongoose
@@ -21,3 +21,19 @@ mongoose
   .then(() => {
     console.log('Database connection successfull!!');
   });
+
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Server is listening to port: ${port}`);
+});
+
+// HANDLING PROMISE REJECETION
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥. Shutting Down.');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
